c0057d14239c0b362c1a13f4057faf51925a0ac6
Matheus Americano <137636772+mattheusamericano@users.noreply.github.com>
2026-08-04 08:33:00 -0300

    ajuste no iam cloudbuild

--- gcp/cloudbuild-worker-pool/iam.tf
+++ gcp/cloudbuild-worker-pool/iam.tf
@@ -1,13 +1,13 @@
-# IAM aditivo: concede roles/cloudbuild.workerPoolUser aos principals informados em
-# worker_pool_users, permitindo o uso do pool a partir de outros projetos/pipelines
-# (ex.: service account do Cloud Build em prj-spoke-modelagem). Aditivo porque o
-# consumo do pool é feito por identidades externas ao módulo, não pelo owner do recurso.
-resource "google_cloudbuild_worker_pool_iam_member" "worker_pool_user" {
+resource "google_project_iam_member" "worker_pool_user" {
   for_each = local.worker_pool_user_bindings
-
-  project     = google_cloudbuild_worker_pool.this[each.value.key].project
-  location    = google_cloudbuild_worker_pool.this[each.value.key].location
-  worker_pool = google_cloudbuild_worker_pool.this[each.value.key].name
-  role        = "roles/cloudbuild.workerPoolUser"
-  member      = each.value.member
+ 
+  project = google_cloudbuild_worker_pool.this[each.value.key].project
+  role    = "roles/cloudbuild.workerPoolUser"
+  member  = each.value.member
+ 
+  condition {
+    title       = "restrict-to-${each.value.key}"
+    description = "Restringe roles/cloudbuild.workerPoolUser ao worker pool ${each.value.key}"
+    expression  = "resource.name == \"${google_cloudbuild_worker_pool.this[each.value.key].id}\""
+  }
 }
